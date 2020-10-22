43a47b6e81cdb4c155d8d38349dd357fdad5ea03
Yfer <yago@codeby.com.br>
2020-10-22 11:13:02 -0300

    React && GraphQL App

---
+++ public/precache-manifest.cd52caf9eb5c33c810c601408256320d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c74f584fb9d0aad8e9d124d673d1c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dff12a44858396759c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "21d20f06dfdd564e1ba7",
+    "url": "/static/js/2.11734569.chunk.js"
+  },
+  {
+    "revision": "8c0d193d40bf578a0ef20e74b4598327",
+    "url": "/static/js/2.11734569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dff12a44858396759c5",
+    "url": "/static/js/main.8d8efbf4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "37a39b35e699355acde6016e5ce69039",
+    "url": "/static/media/spacex-logo.37a39b35.png"
+  }
+]);
